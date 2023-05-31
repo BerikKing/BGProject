ed7034bb4c1da215c184d588341a087ffa5aaa2a
Berik <berloo24@gmail.com>
2023-05-31 17:06:49 +0600

    mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,23 @@ let menu = document.querySelector(".mobile_menu");
 let hamburger1 = document.querySelector(".humburger-1");
 let hamburger2 = document.querySelector(".humburger-2");
 let hamburger3 = document.querySelector(".humburger-3");
+let show = document.querySelector('.header_inner_mobile_menu');
+let main = document.querySelector('.main');
+let inner = document.querySelector('.header_inner')
 
-menu.addEventListener('click', () => {
+menu.addEventListener('click', (e) => {
+    show.classList.toggle('show')
     hamburger1.classList.toggle('active');
     hamburger2.classList.toggle('active');
     hamburger3.classList.toggle('active');
     
+})
+window.addEventListener("click", (e) => {
+    if(e.target === main) {
+        show.classList.remove('show')
+        hamburger1.classList.remove('active');
+        hamburger2.classList.remove('active');
+        hamburger3.classList.remove('active');
+    }
+    // console.log(e.target);
 })
